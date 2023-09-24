d97602718f975c7219650691bbfe6c4278706950
Cesar M. Urdaneta <upcesar@gmail.com>
2023-09-23 22:01:09 -0300

    Added full name to user entity

--- src/TravelManagement.Domain/Entities/User.cs
+++ src/TravelManagement.Domain/Entities/User.cs
@@ -5,5 +5,6 @@ namespace TravelManagement.Domain.Entities;
 public class User : Entity
 {
     public string Email { get; init; }
+    public string FullName { get; set; }
     public string Password { get; init; }
 }
