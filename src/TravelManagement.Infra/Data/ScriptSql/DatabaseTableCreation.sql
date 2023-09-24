abf0b119853586c1bbf910a8bf3ff9bd32759d99
Cesar M. Urdaneta <upcesar@gmail.com>
2023-09-24 18:37:01 -0300

    Field adjustments

--- src/TravelManagement.Infra/Data/ScriptSql/DatabaseTableCreation.sql
+++ src/TravelManagement.Infra/Data/ScriptSql/DatabaseTableCreation.sql
@@ -10,9 +10,9 @@ IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Users' AND
 
 CREATE TABLE Users (
        Id                             uniqueidentifier     NOT NULL     PRIMARY KEY                  DEFAULT NEWID(),
-       FullName                       varchar(10)          NOT NULL,
-	   Email                          int                  NOT NULL,
-       [Password]                     varchar(10)          NOT NULL                                  DEFAULT 0,
+       FullName                       varchar(100)         NOT NULL,
+	   Email                          varchar(255)         NOT NULL,
+       [Password]                     varchar(MAX)         NOT NULL,
 	   CreatedAt                      datetimeoffset(7)    NOT NULL                                  DEFAULT GETUTCDATE(),
        UpdatedAt                      datetimeoffset(7)        NULL,
 );	
